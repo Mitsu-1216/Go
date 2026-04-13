d9bb94ebb0d6b13b0d9cc7ff5784a28164e34363
Mitsu-1216 <91414910+Mitsu-1216@users.noreply.github.com>
2026-04-13 22:52:59 +0900

    add Go examples for JSON and method receivers

---
+++ AutomaticConversion/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type Shape struct {
+	X, Y float64
+}
+
+// Pointer receiver method: doubles X.
+func (s *Shape) Double() {
+	s.X *= 2
+}
+
+// Value receiver method: prints X.
+func (s Shape) PrintX() {
+	fmt.Println(s.X)
+}
+
+// Regular function that requires a pointer.
+func DoubleX(s *Shape) {
+	s.X *= 2
+}
+
+func main() {
+	v := Shape{3, 4}
+	p := &v
+
+	// Pointer receiver methods can be called on values.
+	v.Double()
+	(&v).Double()
+
+	// Value receiver methods can be called on pointers.
+	p.PrintX()
+	(*p).PrintX()
+
+	// DoubleX(v) // compile error
+	DoubleX(&v)
+}
